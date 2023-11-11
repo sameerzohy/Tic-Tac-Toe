b6cbbd42bf5d43a990c572826380adf8d66841b6
sameerzohy <sameerzohan65@gmail.com>
2023-11-11 21:20:09 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ function clickBoard(e){
     console.log(boxes);
     checkWinner();
     checkDraw();
-    if(checkDraw() == true){
+    if(checkDraw() == true && board.style.display != 'none'){
         board.style.display = 'none';
         message.textContent = 'The Match is Drawn.';
         message.style.display = 'block';
@@ -35,11 +35,13 @@ function checkWinner(){
 }
 
 function rowWinner(){
+    
     if (boxes[0].textContent === boxes[1].textContent && boxes[1].textContent === boxes[2].textContent && boxes[0].textContent !== ""){
         board.style.display = 'none';
         message.textContent = `${boxes[1].textContent} is the winner`;
         message.style.display = 'block';
         clear.style.display = 'none';
+
     }
     if (boxes[3].textContent === boxes[4].textContent && boxes[4].textContent === boxes[5].textContent && boxes[4].textContent !== ""){
         board.style.display = 'none';
